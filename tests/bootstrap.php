18cbf1516f46252615eb7621ddcb9eb72bdb71c9
ADmad <admad.coder@gmail.com>
2016-12-04 19:04:34 +0530

    Fix testsuite

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,13 @@
 <?php
-/**
- * Test suite bootstrap.
+use Cake\Core\Plugin;
+
+/*
+ * Test suite bootstrap
  *
  * This function is used to find the location of CakePHP whether CakePHP
  * has been installed as a dependency of the plugin, or the plugin is itself
  * installed as a dependency of an application.
  */
-
 $findRoot = function ($root) {
     do {
         $lastRoot = $root;
@@ -15,18 +16,11 @@ $findRoot = function ($root) {
             return $root;
         }
     } while ($root !== $lastRoot);
-
-    throw new Exception("Cannot find the root of the application, unable to run tests");
+    throw new Exception('Cannot find the root of the application, unable to run tests');
 };
 $root = $findRoot(__FILE__);
-$path = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR;
 unset($findRoot);
-
 chdir($root);
-if (file_exists($root . '/config/bootstrap.php')) {
-    require $root . '/config/bootstrap.php';
-}
-
 require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
 
-\Cake\Core\Plugin::load('Muffin/Tokenize', ['path' => $path]);
+Plugin::load('ADmad/JwtAuth', ['path' => dirname(dirname(__FILE__)) . DS]);
