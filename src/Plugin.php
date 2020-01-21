0329bbbb81598fff76746d8792515cde5d81b46e
ADmad <admad.coder@gmail.com>
2020-01-21 08:45:40 +0530

    Update .editorconfig

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,7 +5,10 @@ namespace Muffin\Tokenize;
 
 use Cake\Console\CommandCollection;
 use Cake\Core\BasePlugin;
+use Cake\Core\Configure;
+use Cake\Routing\RouteBuilder;
 use Muffin\Tokenize\Command\ClearTokensCommand;
+use Muffin\Tokenize\Model\Entity\Token;
 
 /**
  * Plugin class for tokenize
@@ -18,6 +21,35 @@ class Plugin extends BasePlugin
      * @var string
      */
     protected $name = 'Tokenize';
+
+    /**
+     * This plugin doesn't need any bootstrapping.
+     *
+     * @var bool
+     */
+    protected $bootstrapEnabled = false;
+
+    /**
+     * Add routes for the plugin.
+     *
+     * @param \Cake\Routing\RouteBuilder $routes The route builder to update.
+     * @return void
+     */
+    public function routes(RouteBuilder $routes): void
+    {
+        $routes->plugin('Muffin/Tokenize', [], function (RouteBuilder $routes) {
+            $length = Configure::read('Muffin/Tokenize.length') ?: Token::DEFAULT_LENGTH;
+            $defaults = [
+                'controller' => 'Tokens',
+                'action' => 'verify',
+            ];
+            $options = [
+                'token' => '[a-zA-Z0-9]{' . $length .'}',
+                'pass' => ['token'],
+            ];
+            $routes->connect('/verify/:token', $defaults, $options);
+        });
+    }
 
     /**
      * Add console commands for the plugin.
